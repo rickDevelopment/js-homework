15e47aa2fba7c80cbdaa09fc1ee19f490816d8e0
Alrick Chalar <alrick.chalar@gmail.com>
2017-05-16 15:17:12 -0400

    compled the 99 bottles assignment, added while loop and if statement

--- 02/99Bottles.js
+++ 02/99Bottles.js
@@ -1,15 +1,14 @@
-var beer = 100; // Amout of beers to start out
+var beer = 99; // Amout of beers to start out
 
-
-while( beer > 0){
-    beer--;
-    if (beer === 0 ){
-        console.log("No more bottles of beer on the wall, no more bottles of beer. ");
-        console.log("Go to the store and buy some more, 99 bottles of beer on the wall.");
-    }
+// r
+while( beer >= 1){
 console.log(beer, "bottles of beer on the wall,", beer, "bottles of beer.");
-
- // subtracts 1 from the number of beers that are on the wall
 console.log("Take one down and pass it around," , beer," bottles of beer on the wall");
+ // subtracts 1 from the number of beers that are on the wall
+    beer--;
 }
 
+if (beer == 0 ){
+    console.log("No more bottles of beer on the wall, no more bottles of beer. ");
+    console.log("Go to the store and buy some more, 99 bottles of beer on the wall.");
+    }
