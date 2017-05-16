64ad370a4d69ad685729640e747491ab9629dca2
Alrick Chalar <alrick.chalar@gmail.com>
2017-05-16 10:54:39 -0400

    created file name 99Bottles.js

---
+++ 02/99Bottles.js
@@ -0,0 +1,15 @@
+var beer = 100; // Amout of beers to start out
+
+
+while( beer > 0){
+    beer--;
+    if (beer === 0 ){
+        console.log("No more bottles of beer on the wall, no more bottles of beer. ");
+        console.log("Go to the store and buy some more, 99 bottles of beer on the wall.");
+    }
+console.log(beer, "bottles of beer on the wall,", beer, "bottles of beer.");
+
+ // subtracts 1 from the number of beers that are on the wall
+console.log("Take one down and pass it around," , beer," bottles of beer on the wall");
+}
+
