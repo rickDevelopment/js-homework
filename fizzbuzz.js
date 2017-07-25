cbe4cb37329fb1612d415bcce69b67d706272df8
Alrick Chalar <alrick.chalar@gmail.com>
2017-07-24 22:26:02 -0400

    added new fizz buzz file

---
+++ fizzbuzz.js
@@ -0,0 +1,26 @@
+// var num =1;
+for(var i = 1; i<=100; i++){
+    if(i % 3 == 0 ){
+        console.log("fiz");
+    }else if
+        (i % 5 == 0){
+        console.log("buzz");
+    }else{
+        console.log("not fizz or buzz")
+    }
+    // num = i;
+    // console.log(num);
+}
+
+// console.log("Function starts");
+
+// function fizzbuzz(){
+//     for(var i = 1; i<=100; i++){
+//         if(i % 3 == 0){
+//             console.log("fizz")
+
+//         }else{
+//             console.log("buzz")
+//         }
+//     }
+// fizzbuzz();
