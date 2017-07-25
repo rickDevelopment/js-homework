08aabe9936c7a54cf5402ff3fd2d3f588b51d307
Alrick Chalar <alrick.chalar@gmail.com>
2017-07-25 10:50:27 -0400

    Udated the fizz app where it runs the funtion however it does not display the result on the page only in the log git status

--- 07/app.js
+++ 07/app.js
@@ -1,4 +1,3 @@
-
 /*Write a program that prints the numbers from 1 to 100.
 But for multiples of three print “Fizz” instead of the number
 and for the multiples of five print “Buzz”.
@@ -19,53 +18,60 @@ For numbers which are multiples of both three and five print “FizzBuzz*/
 
 // }
 
-window.onload = function(){
+window.onload = function() {
     // alert("The Page has loaded");
 
     var heading1 = document.createElement("h1");
-    var headingText = document.createTextNode("Wow! I just created text in my html by using javaScript");
+    var headingText = document.createTextNode("Welcome to the Fizz Buzz App");
 
-   heading1.appendChild(headingText);
-   document.body.appendChild(heading1);
+    heading1.appendChild(headingText);
+    document.body.appendChild(heading1);
 
-// Create a new paragraph and append a welcome text
-   var para1 = document.createElement("p");
-   var para1Text = document.createTextNode("Welcome to the FizzBuzz game!");
+    // Create a new paragraph and append a welcome text
+    var para1 = document.createElement("p");
+    var para1Text = document.createTextNode(results);
 
-   para1.appendChild(para1Text);
-   document.body.appendChild(para1);
-
-//================================================//
+    para1.appendChild(para1Text);
+    document.body.appendChild(para1);
 
 
+    //================================================//
+
+    var results
 
 
-     // function fizzbuzz ()  {
-   //          for (var i = 1; i <= 100; i++) {
-   //       if (i % 3 == 0 && i % 5 == 0 ){
-   //          console.log("FizzBuzz " + i )
-   //       }
-   //       if(i % 3 == 0){
-   //          console.log("Fizz " + i);
-   //       }
-   //       else if(i%5 == 0){
-   //          console.log("Buzz "+ i);
-   //       }else{
-   //          console.log(i);
-   //       }
 
-   //      }
-   //  }// end of the fizzbuzz app
+    function fizzbuzz() {
+        for (var i = 1; i <= 100; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                console.log("Fizz Buzz " + i)
+                results = ("Fizz Buzz" + i)
 
-var button1 = document.getElementById('buzz');
-    button1.onclick = function(event){
+            }
+            if (i % 3 == 0) {
+                console.log("Fizz " + i);
+                results = ("Fizz " + i)
+            } else if (i % 5 == 0) {
+                console.log("Buzz " + i);
+                results = (" Buzz" + i)
+            } else {
+                console.log(i);
+            }
+
+        }
+        return results
+    } // end of the fizzbuzz app
+
+    var $inputValue = document.getElementById('#input_value')
+
+
+    console.log("this is re results" + results)
+
+    var button1 = document.getElementById('buzz');
+    button1.onclick = function(event) {
         var clickedEvent =
 
-        alert("button was clicked")
+            console.log(fizzbuzz());
     }
 
 }
-
-
-
-
